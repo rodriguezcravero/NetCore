bbd000a88f725de67890c9d8f36b6b199606f9d7
rodriguezcravero <nacho.rodriguez85@gmail.com>
2020-03-19 17:05:25 -0300

    primer commit

---
+++ proyectosDotnet/teoria1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace teoria1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            /*double r1 = 17 / 3;
+            double r2 = 17 / 3.0;
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+
+            int x = 0;
+            int y = 5;
+
+            Console.WriteLine(x != 0 && y/x == 2);
+            Console.WriteLine(x != 0 & y/x == 2);
+            Console.WriteLine(x == 0 || y/x == 2);
+            Console.WriteLine(x == 0 | y/x == 2);
+
+            int x = 1;
+
+            Console.WriteLine(x++);
+            Console.WriteLine(x);
+            
+            Console.WriteLine(++x);
+            Console.WriteLine(x);
+
+            int x = 10;
+            int y = x--; //post decremento
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+            y = --x; //pre decremento
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+
+            Console.WriteLine("Ingrese su nombre por favor");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola" + nombre);
+            Console.ReadKey();
+
+            Console.WriteLine("Hello ");
+            Console.ReadKey();
+            Console.WriteLine("World!");
+
+            string str = "Hola \v \v que \v haces \v Todo bien?";
+
+            Console.WriteLine(str);
+
+            string​ ​st​ = ​@"c:\windows\system";
+            Console​.WriteLine​(st​);*/
+
+            
+
+        }
+    }
+}
